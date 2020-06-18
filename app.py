81c0a4f341a210d44b43a15ae93b1861215803a5
albinTch <atchopba@gmail.com>
2020-06-19 00:05:00 +0200

    include env file

--- app.py
+++ app.py
@@ -12,15 +12,19 @@
 from webbot import Browser
 
 import time
-import config as cfg
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
 
 web = Browser()
 
-web.go_to(cfg.YMAIL_URL)
-web.type(cfg.YMAIL_USER, id="login-username")
+web.go_to(environ.get("YMAIL_URL"))
+web.type(environ.get("YMAIL_USER"), id="login-username")
 web.press(web.Key.ENTER) #
 
 time.sleep(5)
 
-web.type(cfg.YMAIL_PWD, id="login-passwd", classname="password")
+web.type(environ.get("YMAIL_PWD"), id="login-passwd", classname="password")
 web.press(web.Key.ENTER) #
